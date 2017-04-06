74a9ff33a7c4e85f74574bf4cc355047725f5402
AGRAND <aurore@MacBook-Pro-de-Administrateur.local>
2017-04-06 17:10:32 +0200

    control name, first name username

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,21 @@
  * # MainCtrl
  * Controller of the angularTestApp
  */
-angular.module('angularTestApp')
-	.controller('MainCtrl', function ($scope) {
 
-  	
+// create angular app
+var myApp = angular.module('angularTestApp');
+	
+	// create angular controller
+	myApp.controller('MainCtrl', function ($scope) {
+
+	  	// function to submit the form after all validation has occurred 
+	  	$scope.submitForm = function(isValid) {
+
+	    	// check to make sure the form is completely valid
+	    	if (isValid) {
+	      		alert('votre formulaire est validé');
+	    	}
+
+	  	};
+
     });
